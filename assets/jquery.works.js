42f5324041a112016b3cce716c60bdefa7f68562
Eric Windmill <ericwindmill@gmail.com>
2017-06-28 12:54:30 -0700

    Add contact and a bunch of styling to Protfolio

--- assets/jquery.works.js
+++ assets/jquery.works.js
@@ -1,12 +1,10 @@
 $(() => {
 
   $('.works-page ul li').hover(function () {
+    $('.mouseover').css('display', 'none')
     let thisDescription = $(`#${this.id}-description`)
     thisDescription.stop().fadeTo('50', 1.0)
-  }, function () {
-    let thisDescription = $(`#${this.id}-description`)
-    thisDescription.stop().fadeTo('50', 0.0)
-    $('.mouseover').css('display', 'none')
-  })
+
+   })
 })
 
