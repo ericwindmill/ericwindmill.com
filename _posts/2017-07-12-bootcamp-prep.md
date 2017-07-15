643d41f2ffe235e40e766e4ce5aa59ca52e0ef44
Eric Windmill <ericwindmill@gmail.com>
2017-07-14 21:35:16 -0700

    Finish selected work

---
+++ _posts/2017-07-12-bootcamp-prep.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "The Exact Algorithms You Need to Practice to Get Into App Academy or HackReactor "
+date:   2017-07-12 19:10:08 -0700
+categories: bootcamp algorithms
+permalink: blog/bootcamp-prep
+---
---
+++ assets/jquery.home.js
@@ -0,0 +1,40 @@
+$(() => {
+  let arrow = document.querySelector('.directions')
+  let content = document.querySelector('.Home--Content')
+  let title = document.querySelector('.Home--Title')
+  let arrowView
+
+  const showArrow = () => {
+    let i = 0
+    let reverse = false
+    arrowView = setInterval(() => {
+      arrow.style.opacity = (i / 10)
+      if (i === 7 && !reverse) {
+        reverse = true
+      } else if (i === 0 && reverse) {
+        reverse = false
+      }
+      if (!reverse) {
+        i++
+      } else {
+        i--
+      }
+    }, 100)
+  }
+
+  let flag = false
+  content.addEventListener('scroll', () => {
+    clearInterval(arrowView)
+    arrow.style.opacity = 0
+    flag = true
+  })
+
+  setTimeout(() => {
+    if (flag) {
+      return
+    } else {
+      showArrow()
+    }
+  }, 4000)
+})
+
