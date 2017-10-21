705f8d77ff92d2cb4dcc51f3e40918bc83a3b925
Eric Windmill <ericwindmill@gmail.com>
2017-10-21 07:54:43 -0700

    who knows

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,6 +10,7 @@ class Footer extends Component {
     if (!copyright) {
       return null;
     }
+
     return (
       <footer className="footer">
         <UserLinks config={config} labeled />
@@ -25,7 +26,7 @@ class Footer extends Component {
             Based on{" "}
             <a href="https://github.com/Vagr9K/gatsby-advanced-starter">
               Gatsby Advanced Starter
-            </a>.
+            </a>
           </h4>
         </div>
       </footer>
