d4c4f520966acb6e01002c38ded2e8b9eaf2be59
Eric Windmill <ericwindmill@gmail.com>
2017-10-24 06:07:38 -0700

    Style posts

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -18,36 +18,32 @@ export default class PostTemplate extends React.Component {
     if (!post.id) {
       post.category_id = config.postDefaultCategoryID;
     }
-    const excerpt = postNode.excerpt
-    const timeToRead = postNode.timeToRead
     return (
       <div className='PostTemplate--Container'>
         <Helmet>
           <title>{`${post.title} | ${config.siteTitle}`}</title>
         </Helmet>
         <SEO postPath={slug} postNode={postNode} postSEO />
-
-
+        {/*Content*/}
         <div className="Post--Page">
-
-            {/*hero*/}
-
-            <section className='Hero Post-Hero'>
-              <p className="PostDate">{post.month} {post.year}</p>
-              <h1>{post.title}</h1>
-              <h3>in {post.category}</h3>
-            </section>
-
-            {/*body*/}
+          {/*hero*/}
+          <section className='Hero Post-Hero'>
+            <p className="PostDate">{post.month} {post.year}</p>
+            <h1>{post.title}</h1>
+            <h3>in {post.category}</h3>
+          </section>
+          {/*body*/}
+          <img src={post.cover} className='PostCover' alt='cover'/>
+          <section className='PostBody'>
             <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
-
-            {/*Footer -- Meta*/}
-            <div className="post-meta">
-              <PostTags tags={post.tags} />
-              <SocialLinks postPath={slug} postNode={postNode} />
-            </div>
-            <UserInfo config={config} />
+          </section>
+          {/*Footer -- Meta*/}
+          <div className="PostMeta">
+            <PostTags tags={post.tags} />
+            <SocialLinks postPath={slug} postNode={postNode} />
           </div>
+          <UserInfo config={config} />
+        </div>
 
       </div>
     );
