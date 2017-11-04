24919aa19645cfdbc2f2ecbb16f3089b6c110a3d
Eric Windmill <ericwindmill@gmail.com>
2017-11-04 16:55:51 -0700

    Add netlify form handling

--- src/components/Forms/ContactForm.jsx
+++ src/components/Forms/ContactForm.jsx
@@ -26,7 +26,7 @@ class ContactForm extends Component {
     return (
 
 
-      <form netlify className='ContactForm'>
+      <form netlify netlify-honeypot className='ContactForm'>
         <input type="hidden" name="form-name" value="contact" />
         <input type='text' name='email' placeholder='Your Email' />
         <textarea type='text' onChange={this.resizeTextarea} name='message' placeholder='Your Message' />
