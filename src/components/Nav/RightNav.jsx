f63c3562775c6b94f8bbd6b76230673f37685ed9
Eric Windmill <ericwindmill@gmail.com>
2017-11-20 14:11:20 -0500

    finish new nav

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -1,46 +1,181 @@
-import React, { Component } from "react"
+/* eslint-disable no-param-reassign */
+import React, {Component} from "react"
 import Link from 'gatsby-link'
-import HomeTabs from '../Tabs/HomeTabs'
 import UserLinks from '../UserLinks/UserLinks'
 import config from '../../../data/SiteConfig'
 
 class WideSideNavigation extends Component {
-  whichNav() {
-    if (this.props.path === '/') {
-      return (
-        <div>
-          <nav className='MainNavigation'>
-            <Link className='to-Stream' to='/stream'>Stream</Link>
-            <Link className='to-Courses' to='/courses'>Courses</Link>
-            <Link className='to-About' to='/about'>About</Link>
-            <Link className='to-Contact' to='/contact'>Contact</Link>
-          </nav>
-          <HomeTabs postEdges={this.props.postEdges} />
-        </div>
-      )
-    } else {
-      return (
-        <div className='WideNavigation'>
-          <nav>
-            <Link className='to-About' to='/'>Home</Link>
-            <Link className='to-Stream' to='/stream'>Stream</Link>
-            <Link className='to-Courses' to='/courses'>Courses</Link>
-            <Link className='to-Courses' to='/about'>About</Link>
-            <Link to='/opt-in'>Mailing List</Link>
-            <Link className='to-Contact' to='/contact'>Contact</Link>
-          </nav>
-          <footer>
-            <h2>Elsewhere</h2>
-            <div className='Hero--SocialLinks'><UserLinks config={config} /></div>
-            <p>I'm currently available for small projects.</p>
-          </footer>
-        </div>
-      )
+  constructor(props) {
+    super(props)
+
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+
+  // check to see what page you're on to start
+  componentDidMount() {
+    // const nav = document.querySelector("nav")
+    if (this.props.path !== '/') {
+      const homeLink = document.querySelector('.to-Home')
+      const streamLink = document.querySelector('.to-Stream')
+      const courseLink = document.querySelector('.to-Courses')
+      const aboutLink = document.querySelector('.to-About')
+      const optInLink = document.querySelector('.to-OptIn')
+      const contactLink = document.querySelector('.to-Contact')
+      const footer = document.querySelector('footer')
+      const nav = document.querySelector("nav")
+      const permas = document.querySelectorAll(".permalink")
+
+      // adjust width
+      nav.style.width = '300px'
+
+      // rotate links
+      permas.forEach(link => {
+        link.style.transform = "rotate(0deg)"
+      })
+
+      // position links
+      homeLink.style.top = '225px'
+      homeLink.style.left = '50px'
+
+      streamLink.style.top = '250px'
+      streamLink.style.left = '50px'
+
+      courseLink.style.top = '275px'
+      courseLink.style.left = '50px'
+
+      aboutLink.style.top = '300px'
+      aboutLink.style.left = '50px'
+
+      optInLink.style.top = '325px'
+      optInLink.style.left = '50px'
+
+      contactLink.style.top = '350px'
+      contactLink.style.left = '50px'
+
+      footer.style.bottom = "0px"
     }
   }
 
-  render () {
-    return this.whichNav();
+  // 1. Create all the Links / Container Divs
+  // The ones that should be hidden on home page begin with 'home-hidden' class
+  navFoundation() {
+    return (
+      <nav className='navigation'>
+        <Link
+          onClick={this.handlePageChange}
+          className='to-Home navEl home-revealed toggle-link'
+          to='/'
+        >Home</Link>
+        <Link
+          onClick={this.handlePageChange}
+          className='to-Stream navEl home-revealed permalink'
+          to='/stream'
+        >Stream</Link>
+        <Link
+          onClick={this.handlePageChange}
+          className='to-Courses navEl home-revealed permalink'
+          to='/courses'
+        >Courses</Link>
+        <Link
+          onClick={this.handlePageChange}
+          className='to-About navEl home-revealed permalink'
+          to='/about'
+        >About</Link>
+        <Link
+          onClick={this.handlePageChange}
+          className='to-OptIn navEl home-hidden toggle-link'
+          to='/opt-in'
+        >Mailing List</Link>
+        <Link
+          onClick={this.handlePageChange}
+          className='to-Contact navEl home-hidden permalink'
+          to='/contact'
+        >Contact</Link>
+        <footer className='home-hidden navEl toggle-link'>
+          <h2>Elsewhere</h2>
+          <div className='Hero--SocialLinks'><UserLinks config={config}/></div>
+          <p>I'm currently available for small projects.</p>
+        </footer>
+      </nav>
+    )
+  }
+
+
+  handlePageChange(e) {
+    const homeLink = document.querySelector('.to-Home')
+    const streamLink = document.querySelector('.to-Stream')
+    const courseLink = document.querySelector('.to-Courses')
+    const aboutLink = document.querySelector('.to-About')
+    const optInLink = document.querySelector('.to-OptIn')
+    const contactLink = document.querySelector('.to-Contact')
+    const footer = document.querySelector('footer')
+    const nav = document.querySelector("nav")
+    const permas = document.querySelectorAll(".permalink")
+
+    if (!e.target.classList.contains("to-Home")) {
+       // adjust width
+      nav.style.width = '300px'
+
+      // rotate links
+      permas.forEach(link => {
+        link.style.transform = "rotate(0deg)"
+      })
+
+      // position links
+      homeLink.style.top = '225px'
+      homeLink.style.left = '50px'
+
+      streamLink.style.top = '250px'
+      streamLink.style.left = '50px'
+
+      courseLink.style.top = '275px'
+      courseLink.style.left = '50px'
+
+      aboutLink.style.top = '300px'
+      aboutLink.style.left = '50px'
+
+      optInLink.style.top = '325px'
+      optInLink.style.left = '50px'
+
+      contactLink.style.top = '350px'
+      contactLink.style.left = '50px'
+
+      footer.style.bottom = "0px"
+
+    } else {
+      // adjust width
+      nav.style.width = '55px'
+
+      // rotate links
+      permas.forEach(link => {
+        link.style.transform = "rotate(90deg)"
+      })
+
+      // position links
+      homeLink.style.top = '225px'
+      homeLink.style.left = '1000px'
+
+      streamLink.style.top = '20%'
+      streamLink.style.left = '0px'
+
+      courseLink.style.top = '40%'
+      courseLink.style.left = '0px'
+
+      aboutLink.style.top = '60%'
+      aboutLink.style.left = '0px'
+
+      optInLink.style.top = '325px'
+      optInLink.style.left = '1500px'
+
+      contactLink.style.top = '80%'
+      contactLink.style.left = '0px'
+
+      footer.style.bottom = "-400px"
+      }
+    }
+
+  render() {
+    return this.navFoundation();
   }
 }
 
